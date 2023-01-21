a9ea403d655f749182f5a3428dfd0272a8ce0f52
Youngseo Choi <ys550499@gmail.com>
2023-01-21 15:49:24 +0900

    추가, 수정 삭제 버튼 추가 & 삭제 기능 완료

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,5 +1,22 @@
-$(document).ready(() => {
-  $(".check-all").click(() => {
-    $("input.check").prop("checked",$(".check-all").prop("checked"));
+$(document).ready(function () {
+
+  $(".js-check-all").click(function () {
+    $("input.js-check").prop("checked", $(".js-check-all").prop("checked"));
   })
+
+  $(".js-delete-todo").click(async function () {
+    const tasks = []
+    $("input.js-check:checked").each(function () {
+      tasks.push(
+          $.ajax({
+            url: `/api/todos/${$(this).val()}`,
+            method: "DELETE",
+            dataType: "json",
+          })
+      )
+    })
+    await Promise.allSettled(tasks)
+    location.reload()
+  })
+
 })
