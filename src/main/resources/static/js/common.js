e2242fdd63186b6aa34c340e6a63ec7a08686d19
Youngseo Choi <ys550499@gmail.com>
2023-01-21 18:02:26 +0900

    수정 기능 완료

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
     $("input.js-check").prop("checked", $(".js-check-all").prop("checked"));
   })
 
+  $(".js-create-todo").click(async function () {
+    const $content = $(this).closest(".js-modal-content").find(".js-input-content")
+    const data = {
+      content: $content.val()
+    }
+    await $.ajax({
+      url: `/api/todos`,
+      method: "POST",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+    })
+    location.reload()
+  })
+
   $(".js-delete-todo").click(async function () {
     const tasks = []
     $("input.js-check:checked").each(function () {
@@ -18,18 +32,28 @@ $(document).ready(function () {
     location.reload()
   })
 
-  $(".js-create-todo").click(async function () {
-    const $content = $(this).closest(".js-modal-content").find(".js-input-content")
-    const data = {
-      content: $content.val()
+  $(".js-update-todo").click(async function () {
+    const buttonText = $(this).text()
+    if (buttonText !== "수정완료") {
+      $(this).text("수정완료")
+      const $tdContent = $(this).closest("tr").children("td:eq(1)")
+      const before = $tdContent.text()
+      const child = `<input type="text" class="form-control js-input-content" value="${before}"/>`
+      $tdContent.text("").append(child)
+    } else {
+      const id = $(this).val()
+      const $content = $(this).closest("tr").find(".js-input-content")
+      const data = {
+        content: $content.val()
+      }
+      await $.ajax({
+        url: `/api/todos/${id}`,
+        method: "PUT",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+      })
+      location.reload()
     }
-    await $.ajax({
-      url: `/api/todos`,
-      method: "POST",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-    })
-    location.reload()
   })
 
 })
