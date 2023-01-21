cd4235bd01098cd9bd9035018bfc4e574b209e3f
Youngseo Choi <ys550499@gmail.com>
2023-01-21 17:22:25 +0900

    추가 기능 완료

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS todo (
     content TEXT
 );
 
-INSERT INTO todo
-VALUES (1, '밥 먹기'),
-       (2, '공부하기'),
-       (3, 'Play the piano');
+INSERT INTO todo (content)
+VALUES ('밥 먹기'),
+       ('공부하기'),
+       ('Play the piano');
--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -11,11 +11,24 @@ $(document).ready(function () {
           $.ajax({
             url: `/api/todos/${$(this).val()}`,
             method: "DELETE",
-            dataType: "json",
           })
       )
     })
     await Promise.allSettled(tasks)
+    location.reload()
+  })
+
+  $(".js-create-todo").click(async function () {
+    const $content = $(this).closest(".js-modal-content").find(".js-input-content")
+    const data = {
+      content: $content.val()
+    }
+    await $.ajax({
+      url: `/api/todos`,
+      method: "POST",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+    })
     location.reload()
   })
 
